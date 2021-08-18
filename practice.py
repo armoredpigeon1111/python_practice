65c62757fb6482d8775bbfdebe6c900c5c449da9
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-18 15:28:41 -0400

    Added run_get_triangle_area and get_triangle_area functions

--- practice.py
+++ practice.py
@@ -1,3 +1,8 @@
+#Title: Python Practice Problems
+#Author: Richard Fleming
+#Create Date: August 18, 2021
+
+#PROBLEM 1 - Calculate Minutes to seconds
 def run_calculate_mins_to_secs():
     minutes = int(input("Enter minutes to get seconds: "))
     seconds = calculate_mins_to_secs(minutes)
@@ -7,4 +12,19 @@ def calculate_mins_to_secs(minutes):
     seconds = minutes * 60
     return seconds
 
-run_calculate_mins_to_secs()
+#run_calculate_mins_to_secs()
+
+
+
+#PROBLEM 2 - Area of a Triangle
+def run_get_triangle_area():
+    triangle_height = int(input("Enter the height of the triangle: "))
+    triangle_base = int(input("Enter the base length of the triangle: "))
+    triangle_area = get_triangle_area(triangle_height, triangle_base)
+    print(f'The area of a triangle with {triangle_base} base length and {triangle_height} height is {triangle_area}.')
+
+def get_triangle_area(height, base):
+    area = (base * height) / 2
+    return area
+
+run_get_triangle_area()
